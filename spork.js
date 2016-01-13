c077decf22099406b035effa1dbe3a62a73cd970
Hans Felde <m33pfish@gmail.com>
2016-01-13 01:10:52 -0600

    Update spork.js

--- spork.js
+++ spork.js
@@ -1,1 +1,6 @@
-
+var spork = {}
+spork.extendor = extA => extB => (Object.keys(extA).forEach(k => extB[k] = extA[k]), extB)
+spork.extendee = extB => extA => (Object.keys(extA).forEach(k => extB[k] = extA[k]), extB)
+spork.filter = o => t => !!Math.min.apply(this, Object.keys(o).map(k => o[k](t[k])))
+spork.is = x => v => v == x
+spork.staticize = o => (s => (Object.keys(o).map(k => s[k] = o[k]()), s))({})
